3b5535b5c65097462b7f1543c876d1468c5055a1
XuanRan <59474133+XuanRanDev@users.noreply.github.com>
2022-11-13 13:30:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ pushMessage()
 
 # PushPlus
 def pushMessage():
-    url = 'http://www.pushplus.plus/send?token=' + '1f630c6f480246a2b174dfedb49be0fa' + '&title=' + os.environ.get("USERS", ""  + '&content=' + os.environ.get("USERS", "" + '&template=html'
+    url = 'http://www.pushplus.plus/send?token=' + '1f630c6f480246a2b174dfedb49be0fa' + '&title=' + os.environ.get("USERS", "")  + '&content=' 
+                                                                                                                   + os.environ.get("USERS", "") + '&template=html'
     resp = requests.post(url)
     if resp.json()["code"] == 200:
         print('推送消息提醒成功！')
